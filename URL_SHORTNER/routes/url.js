3680ebf6519267ed1cc1ce5b9ea80df3af52054b
Ritesh Kushwaha <riteshkushwaha497@gmail.com>
2025-02-14 00:18:13 +0530

    Delete URL_SHORTNER directory

--- URL_SHORTNER/routes/url.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { generateNewURL } = require('../controllers/url');
-const router = express.Router();
-
-router.post('/', generateNewURL);
-
-module.exports = router;
